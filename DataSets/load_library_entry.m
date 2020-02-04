7b3615b7421b932def6bb35cbef8bfd1ce3fd5d5
Nathan Bamberger <38992219+nbamberger@users.noreply.github.com>
2020-02-04 10:48:24 -0700

    Program to load-in a dataset listed in the dataset library

---
+++ DataSets/load_library_entry.m
@@ -0,0 +1,30 @@
+%NDB 07May19: Loads a single trace structure listed in the dataset library
+%given its ID number
+function TraceStruct = load_library_entry(dataset_library, id2load)
+
+    %Read in from the LabMonti library
+    if isfield(dataset_library,'path_name')
+        %Where all the data is located
+        base_path = 'D:\Transport_Data';
+
+        %Where the specific file containing the dataset we are looking for is
+        %located
+        file_path = fullfile(base_path, dataset_library.path_name{id2load},...
+            strcat(dataset_library.file_name{id2load},'.mat'));
+        
+    %Read in from the example library for the public version on GitHub
+    else
+        file_path = fullfile('DataSets',strcat(...
+            dataset_library.file_name{id2load},'.mat'));
+    end
+
+    %Load the file
+    TraceStruct = importdata(file_path);
+    
+    %If the file contains multiple trace structures, select just the one we
+    %want
+    if ~isempty(dataset_library.section_name{id2load})
+        TraceStruct = TraceStruct.(dataset_library.section_name{id2load});
+    end
+
+end
