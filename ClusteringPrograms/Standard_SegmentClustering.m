153973c5d5febc9e50ef1fea85b0936ddaeb9c50
Nathan Bamberger <38992219+nbamberger@users.noreply.github.com>
2020-02-11 12:03:27 -0700

    Added outputs

--- ClusteringPrograms/Standard_SegmentClustering.m
+++ ClusteringPrograms/Standard_SegmentClustering.m
@@ -1,4 +1,5 @@
-function Standard_SegmentClustering(TraceStruct, name, onHPC)
+function [OO_List, TracesUsed] = Standard_SegmentClustering(TraceStruct,...
+    save_name, onHPC)
     %Copyright 2020 LabMonti.  Written by Nathan Bamberger.  This work is 
     %licensed under the Creative Commons Attribution-NonCommercial 4.0 
     %International License. To view a copy of this license, visit 
@@ -14,18 +15,22 @@ function Standard_SegmentClustering(TraceStruct, name, onHPC)
     %TraceStruct: the trace structure containing the dataset to be
     %clustered
     %
-    %name: a name to identify the dataset being clustered
+    %save_name: a name to identify the dataset being clustered in saved
+    %   output files; leave blank/empty to not save output files
     %
     %onHPC: logical variable; if true, parameters are set to include
     %   parallelization for the HPC at Arizona
 
 
-    if nargin < 2
+    if nargin < 3
         onHPC = false;
+    end
+    if nargin < 2
+        save_name = [];
     end
     
     %Get "standard" clustering input parameters
-    [I, minPtsList] = generateClusteringInput_Bamberger2020(name, onHPC);
+    [I, minPtsList] = generateClusteringInput_Bamberger2020(save_name, onHPC);
     
     %Parameters for plotting of clustering solutions
     cutoff_frac = 0.01;
@@ -36,7 +41,7 @@ function Standard_SegmentClustering(TraceStruct, name, onHPC)
         I,minPtsList);
     
     %Save output (to local directory)
-    save(strcat(name,'_ClustOut.mat'),'OO_List','TracesUsed');
+    save(strcat(save_name,'_ClustOut.mat'),'OO_List','TracesUsed');
 
     %Get clustering output for "reference" minPts value
     OO = OO_List{minPts_ID_toPlot}; 
@@ -44,6 +49,6 @@ function Standard_SegmentClustering(TraceStruct, name, onHPC)
 
     %Save reachability plot and "maximum valley clusters" for reference
     %clustering output
-    Show_FullValleyClusters(OO,cutoff_frac,'LinearSegments',name);
+    Show_FullValleyClusters(OO,cutoff_frac,'LinearSegments',save_name);
 
 end
