ac9a47413b4f2a6a2127dafc28d9c6e2ffa20ec5
Nathan Bamberger <38992219+nbamberger@users.noreply.github.com>
2021-04-29 17:23:30 -0700

    Brought fitting routine inside this function so that I can apply restrictions
    I want to restrict the gaussian peak to fall within the data range (this always happened anyway with well-behaved data, the only difference is that this now works better for trickier data)

--- Scripting/IterativePeakFitting/restricted_gaussian_fit.m
+++ Scripting/IterativePeakFitting/restricted_gaussian_fit.m
@@ -10,7 +10,28 @@ function [peak,xdata,yhat] = restricted_gaussian_fit(xdata, ydata, lower_bound, 
     xdata = xdata(xdata > lower_bound);
     ydata = ydata(xdata < upper_bound);
     xdata = xdata(xdata < upper_bound);
-    
-    [yhat,peak] = fit_xyData_nGaussians(xdata,ydata,1,ToPlot);
 
+    %Perform the single-gaussian fit, requiring the gaussina peak to fall
+    %within the data range
+    min_params = [0, lower_bound, 0];
+    max_params = [Inf, upper_bound, Inf];
+    peak_fit = fit(xdata, ydata, 'gauss1', 'Lower', min_params, 'Upper', max_params);
+
+    %Extract best fit parameters
+    a = peak_fit.a1;
+    b = peak_fit.b1;
+    c = peak_fit.c1;  
+
+    %Get peak and fitted curve
+    peak = b;      
+    yhat = a*exp(-((xdata - b)./c).^2);
+    
+    if ToPlot
+        figure();
+        hold on;
+        [x,y] = convert_to_histogram_endpoints(xdata,ydata);
+        plot(x,y);
+        plot(xdata,yhat);
+    end
+    
 end
